4a731dca05c543dc7a8edada5b1a899d15bd9916
sabrinaliau97 <68696498+sabrinaliau97@users.noreply.github.com>
2022-03-25 13:45:15 +0800

    Add files via upload

---
+++ client/src/__mocks__/applyload.js
@@ -0,0 +1,94 @@
+import { v4 as uuid } from 'uuid';
+
+export const applyloan = [
+  {
+    loanid: 1,
+    loanamount: 135532.99, 
+    payamount: 35532.99,
+    appstatus: 'Approved',
+    paystatus:  'Partial Paid',
+    appdate: 1545016400000,
+    paydate: 1555216400000
+  },
+  {
+    loanid: 2,
+    loanamount: 34367.53, 
+    payamount: 34367.53,
+    appstatus: 'Approved',
+    paystatus:  'Fully Paid',
+    appdate: 1345016400000,
+    paydate: 1555216400000
+  },
+  {
+    loanid: 3,
+    loanamount: 45086.74, 
+    payamount: 45086.74,
+    appstatus: 'Approved',
+    paystatus:  'Fully Paid',
+    appdate: 1565016400000,
+    paydate: 1685216400000
+  },
+  {
+    loanid: 4,
+    loanamount: 140123.37, 
+    payamount: 51364.06,
+    appstatus: 'Approved',
+    paystatus: 'Partially Paid',
+    appdate: 1215055500000,
+    paydate: 1575216400000
+  },
+  {
+    loanid: 5,
+    loanamount: 57800.21, 
+    payamount: 47800.99,
+    appstatus: 'Approved',
+    paystatus:'Partially Paid',
+    appdate: 1565016400000,
+    paydate: 1565016400000
+  },
+  {
+    loanid: 6,
+    loanamount: 13165.14, 
+    payamount: 13165.14,
+    appstatus: 'Approved',
+    paystatus: 'Fully Paid',
+    appdate: 156534500000,
+    paydate: 157226400000
+  },
+  {
+    loanid: 7,
+    loanamount: 82062.24, 
+    payamount: 82062.99,
+    appstatus: 'Approved',
+    paystatus:'Fully Paid',
+    appdate: 1515016400000,
+    paydate: 1555216400000
+  },
+  {
+    loanid: 8,
+    loanamount: 12416.32, 
+    payamount: 1226.99,
+    appstatus: 'Approved',
+    paystatus:'Fully paid',
+    appdate: 1475016400000,
+    paydate: 1555216400000
+  },
+  {
+    loanid: 9,
+    loanamount: 17250.83, 
+    payamount: 1250.83,
+    appstatus: 'Approved',
+    paystatus: 'Partially Paid',
+    appdate: 1565016400000,
+    paydate: 17216400000
+  },
+  {
+    loanid: 10,
+    loanamount: 46431.85, 
+    payamount: 17250.83,
+    appstatus: 'Approved',
+    paystatus:  'Partially Paid',
+    appdate: 1127016400000,
+    paydate: 1555216400000
+  },
+];
